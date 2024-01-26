b95c1625d399f653d17a605a309634b493f570dc
yutong-ji <yji@stu.naperville203.org>
2024-01-26 10:15:50 -0600

    got scores from team red and team blue

--- match-history.js
+++ match-history.js
@@ -45,11 +45,14 @@ async function fetchTeam3061PastMatches() {
     console.log(`Past Matches for Team ${teamKey} at ${eventKey}:`);
     console.log(`Match Numbers for Team ${teamKey} at ${eventKey}:`);
     console.log(matchNumbers.join(", "));
-
     pastMatches.forEach((match) => {
       console.log(`Match Number: ${match.match_number}`);
-      console.log(`Alliance: ${match.alliances.red.team_keys.join(", ")}`);
-      console.log(`Alliance: ${match.alliances.blue.team_keys.join(", ")}`);
+      console.log(`Red Alliance: ${match.alliances.red.team_keys.join(", ")} `);
+      console.log(
+        `Blue Alliance: ${match.alliances.blue.team_keys.join(", ")}`
+      );
+      console.log(`Red Score: ${match.alliances.red.score}`);
+      console.log(`Blue Score: ${match.alliances.blue.score}`);
     });
   } catch (error) {
     console.error("Error:", error.message);
