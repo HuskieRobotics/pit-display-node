6ca3f5fbba779972b00f27f65a6e73b0d4d66e5b
Geoff Schmit <gcschmit@gmail.com>
2024-09-07 18:08:07 -0500

    add simple node server for hosting

---
+++ server.js
@@ -0,0 +1,22 @@
+/**
+ * main Javascript file for the application
+ *  this file is executed by the Node server
+ */
+
+// import the express module, which exports the express function
+const express = require("express");
+
+// invoke the express function to create an Express application
+const app = express();
+
+// load environment variables from the .env file into process.env
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+
+// load assets
+app.use(express.static("website"));
+
+// start the server on port 8080
+app.listen(8080, () => {
+  console.log("server is listening on http://localhost:8080");
+});
