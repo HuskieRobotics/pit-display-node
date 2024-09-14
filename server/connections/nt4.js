249b1f7d8c2d7f9c42c9239b8100c036c465eaab
Geoff Schmit <gcschmit@gmail.com>
2024-09-14 17:10:27 -0500

    only attempt NT connections when the robot is local

--- server/connections/nt4.js
+++ server/connections/nt4.js
@@ -3,21 +3,27 @@ const ntTopics = require("../model/ntTopics");
 const { emitTemperatures } = require("../socket/socket");
 const { formatTemperatures } = require("../../views/robot");
 
-const ntCore = nt4Client.NetworkTables.getInstanceByURI("localhost");
+let ntCore;
 
-for (const topic of ntTopics) {
-  if (topic.type === "double") {
-    const ntTopic = ntCore.createTopic(
-      topic.path,
-      nt4Client.NetworkTablesTypeInfos.kDouble
-    );
-    ntTopic.subscribe((value) => {
-      ntTopics.find((t) => t.path === topic.path).value = value;
-      emitTemperatures(formatTemperatures(getMotorTemperatures()));
-    }, true);
-  } else {
-    console.log("Unsupported NT type");
+if (process.env.ROBOT_IS_LOCAL === "true") {
+  ntCore = nt4Client.NetworkTables.getInstanceByURI("localhost");
+
+  for (const topic of ntTopics) {
+    if (topic.type === "double") {
+      const ntTopic = ntCore.createTopic(
+        topic.path,
+        nt4Client.NetworkTablesTypeInfos.kDouble
+      );
+      ntTopic.subscribe((value) => {
+        ntTopics.find((t) => t.path === topic.path).value = value;
+        emitTemperatures(formatTemperatures(getMotorTemperatures()));
+      }, true);
+    } else {
+      console.log("Unsupported NT type");
+    }
   }
+} else {
+  console.log("Robot is not local");
 }
 
 function getMotorTemperatures() {
