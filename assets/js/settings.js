7d40ff3890b2b6bd31ffd90809bb6f9b1c24343f
mmgokhale <117867154+mmgokhale@users.noreply.github.com>
2025-01-17 09:34:47 -0600

    Added prompts to enter the event key and team name. Still need to write javascript code to handle these entries.
    Need some kind of boolean value to toggle between twitch and youtube since the streamservices have different code behind them
    
    Learned that a serverside settings.js is not necessary.

---
+++ assets/js/settings.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("streaming-service");
+  const selectedService = document.getElementById("selected-service");
+  const streamingLink = document.getElementById("streaming-link");
+  const submitButton = document.getElementById("submit-button");
+
+  dropdown.addEventListener("change", function () {
+    selectedService.textContent = `Selected service: ${dropdown.value}`;
+  });
+
+  submitButton.addEventListener("click", function () {
+    // captre the selected streamservice and link
+    const selectedServiceValue = dropdown.value;
+    const streamingLinkValue = streamingLink.value;
+    // log selected streamservice and link bc need to test
+    console.log(`Selected service: ${selectedServiceValue}`);
+    console.log(`Entered link: ${streamingLinkValue}`);
+  });
+});
--- views/settings.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const dropdown = document.getElementById("streaming-service");
-  const selectedService = document.getElementById("selected-service");
-
-  dropdown.addEventListener("change", function () {
-    selectedService.textContent = `Selected service: ${dropdown.value}`;
-  });
-});
