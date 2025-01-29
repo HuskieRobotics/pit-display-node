acc51bd1b0a0f4d931972ed5b5e6c589931851a8
diyagupta05 <diyargupta05@gmail.com>
2025-01-29 10:30:00 -0600

    Worked on debugging
    I cannot get updateChecked to work so I am using the console to work on debugging the actual function to get it to update checked on the right task

--- views/robot.js
+++ views/robot.js
@@ -29,9 +29,8 @@ function getTemperatureColor(temperature) {
 
 // move map code into a new fucntion in routes.js and require and call it
 let newTasks;
-
 function makeTaskObject(tasks) {
-  const newTasks = tasks.map((task) => {
+  newTasks = tasks.map((task) => {
     return {
       name: task.name,
       checklistItems: task.checklistItems.map((item) => {
@@ -44,12 +43,9 @@ function makeTaskObject(tasks) {
   });
   return newTasks;
 }
-console.log(newTasks);
+
 function updateChecked(task) {
-  const taskIndex = newTasks.checklistItems.findIndex(
-    (t) => t.taskName === task.taskText
-  );
-  newTasks.checklistItems[taskIndex].checked = task.isChecked;
+  console.log(newTasks);
 }
 
 module.exports = { formatTemperatures, makeTaskObject, updateChecked };
