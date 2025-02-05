66c4e615636974a6c65da1af41b076bdae7e5757
mmgokhale <117867154+mmgokhale@users.noreply.github.com>
2025-02-05 10:11:26 -0600

    Update MongoDB connection URI environment variable from MONGO_URI to MONGODB_URI

--- server/database/connection.js
+++ server/database/connection.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const con = await mongoose.connect(process.env.MONGO_URI);
+    const con = await mongoose.connect(process.env.MONGODB_URI);
 
     console.log(`MongoDB connected : ${con.connection.host}`);
   } catch (err) {
