c66b7ab7e05a3144ce1e51ecdb6ba011dd557a21
diyagupta05 <diyargupta05@gmail.com>
2025-03-17 09:31:25 -0500

    pt 3

--- assets/js/nexus.js
+++ assets/js/nexus.js
@@ -13,9 +13,9 @@ function openPopup(info) {
     minute: "2-digit",
     timeZone: "America/Chicago",
   });
-  const time = info.match[0].times.estimatedQueueTime;
-  const label = info.match[0].label;
-  const status = info.match[0].status;
+  const time = info.matches[0].times.estimatedQueueTime;
+  const label = info.matches[0].label;
+  const status = info.matches[0].status;
   const val = status.charAt(0).toLowerCase() + status.slice(1);
   const formattedTime = formatter.format(time);
   text.innerHTML = label + " is " + val + " at " + formattedTime + "!";
