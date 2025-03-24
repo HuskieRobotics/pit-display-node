6333f05f6ed7157a4c3a71b1848352fb380088cc
diyagupta05 <diyargupta05@gmail.com>
2025-03-24 09:13:57 -0500

    attempted to do story

--- assets/js/nexus.js
+++ assets/js/nexus.js
@@ -13,12 +13,22 @@ function openPopup(info) {
     minute: "2-digit",
     timeZone: "America/Chicago",
   });
+  if ("3061" in info.matches.redteams || "500" in info.matches.redteams) {
+    popUp.classList.remove("blue-popup");
+    popUp.classList.add("red-popup");
+    console.log("red");
+  }
+  if ("3061" in info.matches.blueteams || "500" in info.matches.blueteams) {
+    popUp.classList.remove("red-popup");
+    popUp.classList.add("blue-popup");
+    console.log("blue");
+  }
   const time = info.matches[info.matches.length - 1].times.estimatedQueueTime;
   const label = info.matches[info.matches.length - 1].label;
   const status = info.matches[info.matches.length - 1].status;
   const val = status.charAt(0).toLowerCase() + status.slice(1);
   const formattedTime = formatter.format(time);
-  text.innerHTML = label + " is " + val + " at " + formattedTime + "!";
+  text.innerHTML = label + " is " + val + " at " + formattedTime;
   popUp.classList.add("open-popup");
 }
 
